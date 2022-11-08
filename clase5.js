bee65cf50bdf7920affe6551e52165ccaddd6f4a
sabrinitus <slstradu@gmail.com>
2022-11-08 19:36:09 -0300

    subo primer cambio

---
+++ clase5.js
@@ -0,0 +1,12 @@
+//function nombreCompleto(nombre, apellido){
+  //   return nombre + " " + apellido;
+//};
+
+   // console.log(nombreCompleto("Lucas","Montoby")
+
+
+//   let suma = (n1, n2) => n1 + n2
+  // let resta = (n1, n2) => n1 - n2
+   //let multi = (n1, n2) => n1 * n2
+// let div = (n1, n2) => n1 / n2
+
